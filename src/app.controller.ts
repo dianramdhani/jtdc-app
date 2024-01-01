2f6cdab028f2e9e5fec6f5937652f7e1e5919be7
Dian Ramdhani Lubis <38197188+dianramdhani@users.noreply.github.com>
2024-01-01 11:16:51 +0700

    v1.4 (#5)
    * init config, swagger
    
    * init browser-manager
    
    * init schedule
    
    * implement cookiesGrabber
    
    * init db
    
    * finish cookiesGrabber
    
    * encapsulate BrowserManager
    
    * finish accounts service
    
    * init checkout
    
    * init checkout
    
    * implement create account multiple
    
    * implement autoLogin
    
    * implement auto co
    
    * extract logic checkout
    
    * implement chrome path
    
    * implement query username-only in find all
    
    * implement file logs
    
    ---------
    
    Co-authored-by: Dian Ramdhani Lubis <dian.lubis@prosa.ai>

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  ParseBoolPipe,
+  Query,
+  Res,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { ApiQuery } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
@@ -8,5 +19,27 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('logs')
+  @ApiQuery({
+    name: 'error-only',
+    required: false,
+  })
+  async getLogs(
+    @Res() res: Response,
+    @Query('error-only', new DefaultValuePipe(false), ParseBoolPipe)
+    errorOnly?: boolean,
+  ) {
+    const logs = await readFile(
+      join(
+        __dirname,
+        '../logs/',
+        errorOnly ? 'stream-error.log' : 'stream-combined.log',
+      ),
+      'utf-8',
+    );
+    res.header('Content-Type', 'text/plain');
+    res.send(logs);
   }
 }
